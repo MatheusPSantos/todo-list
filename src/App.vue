023cabd5c2d3637ba5805a1a24efd275d2ddeb1b
matheuspsantos <matheuspereira8187@gmail.com>
2018-06-01 01:06:46 -0300

    construindo a aplicação, dando ação para os componentes

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="todoapp">
+    <header class="header">
+      <h1>Tarefas</h1>
+      <input-task></input-task>
+    </header>
+  </section>
+</template>
+
+<script>
+import InputTask from './components/InputTask'
+
+export default {
+  name: 'App',
+  components: {
+    InputTask
+  }
+}
+</script>
+
+<style>
+.todoapp {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.todoapp h1 {
+  font-weight: 100;
+  font-size: 32pt;
+}
+</style>
---
+++ src/components/InputTask.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <input class="new-todo" placeholder="o que precisa ser feito?" v-on:keyup.enter="addTask">
+    </div>
+</template>
+
+<script>
+// importando o modelo de Task do Model
+import { Task } from '../models/Task'
+
+export default {
+    data() {
+        return{
+        }        
+    },
+    methods: {
+        addTask($event) {
+            let value = $event.target.value;
+            let task = new Task(); // criando um objeto task
+            task.completed = false;
+            task.title = value;
+            console.log(task);
+        }
+    }
+}
+</script>
+
+<style>
+    .new-todo {
+        border-radius: 3px;
+        padding: 2px 10px;
+        border: #0a7c99 solid 1px;
+        color: #0a7c99;
+        box-shadow: 2px 2px rgba(0,0,0,.1);
+    }
+</style>
