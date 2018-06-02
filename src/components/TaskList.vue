89713cbc16a613e0319c03f5e9f98d534b40529a
matheuspsantos <matheuspereira8187@gmail.com>
2018-06-02 20:44:43 -0300

    adicionando o método de completar tarefa

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,7 +1,8 @@
 <template>
 <ul class="todo-list">
-  <li class="todo" v-for="todo in todoList" :key="todo">
+  <li class="todo" v-for="todo in sortedTasks" :key="todo">
     <div class="view">
+      <input type="checkbox" class="toggle" v-on:click="completeTask(todo)">
       <label>{{ todo.title }}</label>
     </div>
   </li>
@@ -12,19 +13,45 @@
 export default {
   props: ['todoList'],
   computed: {
-    sortedTasks: {
-      get: funtion () {
-        let sorted = this.todoList
-        return sorted.sort(function (a, b) {
-          if(a.title < b.title) return -1
-          if(a.title > b.title) return 1
-          return 0
-        })
-      },
-      set: function (novaLista) {
-        this.todoList.concat(novaLista)
-      }
+    sortedTasks: function () {
+      let sorted = this.todoList
+      return sorted.sort(function (a, b) {
+        if (a.title < b.title) return -1
+        if (a.title > b.title) return 1
+        return 0
+      })
+    },
+  },
+  methods: {
+    completeTask (task) {
+      task.completed = !task.completed
     }
   }
 }
 </script>
+
+<style>
+.todo-list {
+  list-style: none;
+  margin-left: 460px
+}
+
+.todo {
+  width: 300px;
+  height: auto;
+  margin: 10px 0 0 0;
+  padding: 10px 0 10px 0;
+  border: 1px solid rgba(0,0,0,.1);
+  box-shadow: 2px 2px 4px rgba(0,0,0,.3);
+  text-align: center;
+  position: relative;
+  color: rgba(0,0,0,.7);
+}
+
+.view {
+  text-align: left;
+}
+.toggle {
+  margin: 0 20px 0 10px;
+}
+</style>
