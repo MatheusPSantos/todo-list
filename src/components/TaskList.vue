5361af14f1e70b6cc6333516d8ed121e47c0e350
matheuspsantos <matheuspereira8187@gmail.com>
2018-06-01 21:37:25 -0300

    propriedade computada sortedTasks criada e depois alterada com get:

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,15 +1,30 @@
 <template>
 <ul class="todo-list">
-    <li class="todo" v-for="todo in todoList" :key="todo">
-        <div class="view">
-            <label>{{ todo.title }}</label>
-        </div>
-    </li>
+  <li class="todo" v-for="todo in todoList" :key="todo">
+    <div class="view">
+      <label>{{ todo.title }}</label>
+    </div>
+  </li>
 </ul>
 </template>
 
 <script>
 export default {
-    props: ['todoList']
+  props: ['todoList'],
+  computed: {
+    sortedTasks: {
+      get: funtion () {
+        let sorted = this.todoList
+        return sorted.sort(function (a, b) {
+          if(a.title < b.title) return -1
+          if(a.title > b.title) return 1
+          return 0
+        })
+      },
+      set: function (novaLista) {
+        this.todoList.concat(novaLista)
+      }
+    }
+  }
 }
 </script>
