997719b8b05a49f821381f4f6871794654177cb9
matheuspsantos <matheuspereira8187@gmail.com>
2018-06-01 21:36:48 -0300

    propriedade computada sortedTasks criada e depois alterada com get:

--- src/components/InputTask.vue
+++ src/components/InputTask.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <input class="new-todo" placeholder="o que precisa ser feito?" v-on:keyup.enter="addTask">
-    </div>
+  <div>
+    <input class="new-todo" placeholder="o que precisa ser feito?" v-on:keyup.enter="addTask">
+  </div>
 </template>
 
 <script>
@@ -9,29 +9,29 @@
 import { Task } from '../models/Task'
 
 export default {
-    data () {
-        return{
-        }        
-    },
-    methods: {
-        addTask($event) {
-            let value = $event.target.value;
-            let task = new Task() // criando um objeto task
-            task.completed = false
-            task.title = value
-            this.$emit('newTask', task)
-            $event.target.value = ''
-        }
-    }
+	data () {
+		return{
+		}        
+	},
+	methods: {
+		addTask($event) {
+			let value = $event.target.value;
+			let task = new Task() // criando um objeto task
+			task.completed = false
+			task.title = value
+			this.$emit('newTask', task)
+			$event.target.value = ''
+		}
+	}
 }
 </script>
 
 <style>
-    .new-todo {
-        border-radius: 3px;
-        padding: 2px 10px;
-        border: #0a7c99 solid 1px;
-        color: #0a7c99;
-        box-shadow: 2px 2px rgba(0,0,0,.1);
-    }
+	.new-todo {
+		border-radius: 3px;
+		padding: 2px 10px;
+		border: #0a7c99 solid 1px;
+		color: #0a7c99;
+		box-shadow: 2px 2px rgba(0,0,0,.1);
+	}
 </style>
